e271b63d46e1af7141d674585e32415bc0653f18
DMcSweeney <donalmcs1@gmail.com>
2024-04-11 14:24:27 +0100

    Update qc endpoints

--- backend/src/api/database.py
+++ backend/src/api/database.py
@@ -16,7 +16,7 @@ def delete_entry():
     req = request.get_json()
     _id = req['_id']
     coll = req['collection']
-    print("I AM HERE", _id, coll, flush=True)
+    
     from app import mongo
 
     database = mongo.db
@@ -25,5 +25,34 @@ def delete_entry():
     res = make_response(jsonify({
         'message': f'Succesfully deleted entry {_id} in {coll}'
     }), 200)
+
+    return res
+
+
+@bp.route('/api/database/get_qc_report', methods=['GET'])
+def get_gc_report():
+    from app import mongo
+    database = mongo.db
+    project = request.args.get("project")
+    _id = request.args.get("_id") if "_id" in request.args else None
+    print(project, _id, flush=True)
+    if _id is not None:
+        cursor = database.quality_control.find({"project": project, "_id": _id, "qc_report": {"$ne": {}} }, {"series_uuid": 1, "qc_report": 1})
+        message = f'Found report for _id ({_id})'
+    else:
+        cursor = database.quality_control.find({"project": project, "qc_report": {"$ne": {}} }, {"series_uuid": 1, "qc_report": 1})
+        message = f'Found {len(reports)} non-empty qc reports for project ({project})'
+    reports = [(x['series_uuid'], x['qc_report']) for x in cursor]
+
+    if len(reports) == 0:
+        res = make_response(jsonify({
+            'message': "Could not find any reports",
+            "reports": None,
+            }), 200)
+    else:
+        res = make_response(jsonify({
+            'message': message,
+            "reports": reports
+        }), 200)
 
     return res
---
+++ tests/api/database/get_qc_report.sh
@@ -0,0 +1,3 @@
+curl --request GET\
+    --header "Content-Type: application/json"\
+    http://localhost:5001/api/database/get_qc_report?project=Alex_Bladder
